ea9030e25b1232bb73fb015fa45552bbc4ff0dd9
Say Cheong <greymistcube@gmail.com>
2025-12-19 17:26:25 +0900

    Moved welcome message to a file

--- js/terminal.js
+++ js/terminal.js
@@ -74,28 +74,13 @@ $(function () {
 
     term._initialized = true;
 
-    // TODO: Use a nicer default font
-    term.writeln([
-      '    Xterm.js is the frontend component that powers many terminals including',
-      '                           \x1b[3mVS Code\x1b[0m, \x1b[3mHyper\x1b[0m and \x1b[3mTheia\x1b[0m!',
-      '',
-      ' ┌ \x1b[1mFeatures\x1b[0m ──────────────────────────────────────────────────────────────────┐',
-      ' │                                                                            │',
-      ' │  \x1b[31;1mApps just work                         \x1b[32mPerformance\x1b[0m                        │',
-      ' │   Xterm.js works with most terminal      Xterm.js is fast and includes an  │',
-      ' │   apps like bash, vim and tmux           optional \x1b[3mWebGL renderer\x1b[0m           │',
-      ' │                                                                            │',
-      ' │  \x1b[33;1mAccessible                             \x1b[34mSelf-contained\x1b[0m                     │',
-      ' │   A screen reader mode is available      Zero external dependencies        │',
-      ' │                                                                            │',
-      ' │  \x1b[35;1mUnicode support                        \x1b[36mAnd much more...\x1b[0m                   │',
-      ' │   Supports CJK 語 and emoji \u2764\ufe0f            \x1b[3mLinks\x1b[0m, \x1b[3mthemes\x1b[0m, \x1b[3maddons\x1b[0m,            │',
-      ' │                                          \x1b[3mtyped API\x1b[0m, \x1b[3mdecorations\x1b[0m            │',
-      ' │                                                                            │',
-      ' └────────────────────────────────────────────────────────────────────────────┘',
-      ''
-    ].join('\r\n'));
+    // Print welcome text
+    const welcome = files["welcome"];
+    for (const line of welcome) {
+      term.writeln(line);
+    }
 
+    term.writeln('');
     term.writeln('Below is a simple emulated backend, try running `help`.');
 
     // Initial prompt
@@ -105,7 +90,7 @@ $(function () {
     term.onData(e => {
       switch (e) {
         case '\u0003': // Ctrl+C
-          term.write('^C');
+          term.writeln('^C');
           command = '';
           prompt(term);
           break;
