8e1d623533ebb0ae1d5514474340c2be0c52d86c
Say Cheong <greymistcube@gmail.com>
2025-12-21 18:38:42 +0900

    Better implementaion of matrix with colors

--- js/matrix.js
+++ js/matrix.js
@@ -11,14 +11,23 @@ async function runMatrix(term, args) {
   let cols = term.cols;
   const drops = rows;
   const tail = 8;
-  const sleep = 32;
+  const sleep = 64;
   let rain = [];
+
+  function createDrop() {
+    drop = [];
+    while (drop.length < tail) {
+      drop.unshift(Math.floor(Math.random() * 16).toString(16));
+    }
+    return drop;
+  }
 
   function fillRain(fromTop) {
     while (rain.length < drops) {
       rain.push({
         y: fromTop ? 1 : Math.floor(Math.random() * rows + 1),
         x: Math.floor(Math.random() * cols + 1),
+        drop: createDrop()
       });
     }
   }
@@ -26,14 +35,34 @@ async function runMatrix(term, args) {
   fillRain(false);
   while (true) {
     for (let elem of rain) {
-      term.write(`\x1b[${elem.y};${elem.x}H${Math.floor(Math.random() * 16).toString(16)}`);
-      if (elem.y - tail >= 1) {
-        term.write(`\x1b[${elem.y - tail};${elem.x}H `);
+      for (let i = 0; i < tail; i++) {
+        let y = elem.y - i;
+        if (1 <= y && y <= rows) {
+          term.write(`\x1b[${y};${elem.x}H`);
+          if (i === 0) {
+            color = '\x1b[37;1m';
+          } else if (i < tail / 2) {
+            color = '\x1b[32;1m';
+          } else {
+            color = '\x1b[32;2m';
+          }
+
+          if (i === 0) {
+            term.write(`${color}${elem.drop[i]}\x1b[0m`);
+          } else if (i === tail - 1) {
+            term.write('\x1b[0m ');
+          } else {
+            term.write(`${color}${elem.drop[i]}\x1b[0m`);
+          }
+        }
       }
+
       elem.y += 1;
+      elem.drop.pop();
+      elem.drop.unshift(Math.floor(Math.random() * 16).toString(16));
     }
 
-    rain = rain.filter(elem => elem.y <= rows + tail);
+    rain = rain.filter(elem => elem.y < rows + tail);
     fillRain(true);
 
     await new Promise(resolve => setTimeout(resolve, sleep));
