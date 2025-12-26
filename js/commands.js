584e25997cec1c0cea0ef164a37e536f5370191c
Say Cheong <greymistcube@gmail.com>
2025-12-26 14:27:51 +0900

    Simplified runHelp with clickable commands

--- js/commands.js
+++ js/commands.js
@@ -34,40 +34,10 @@ function runWelcome(term, args) {
 
 function runHelp(term, args) {
   const padding = 10;
-  function formatMessage(name, description) {
-    const maxLength = term.cols - padding - 3;
-    let remaining = description;
-    const d = [];
-    while (remaining.length > 0) {
-      // Trim any spaces left over from the previous line
-      remaining = remaining.trimStart();
-      // Check if the remaining text fits
-      if (remaining.length < maxLength) {
-        d.push(remaining);
-        remaining = '';
-      } else {
-        let splitIndex = -1;
-        // Check if the remaining line wraps already
-        if (remaining[maxLength] === ' ') {
-          splitIndex = maxLength;
-        } else {
-          // Find the last space to use as the split index
-          for (let i = maxLength - 1; i >= 0; i--) {
-            if (remaining[i] === ' ') {
-              splitIndex = i;
-              break;
-            }
-          }
-        }
-        d.push(remaining.substring(0, splitIndex));
-        remaining = remaining.substring(splitIndex);
-      }
-    }
-    const message = (
-      `  \x1b[32;1m${name.padEnd(padding)}\x1b[0m ${d[0]}` +
-      d.slice(1).map(e => `\r\n  ${' '.repeat(padding)} ${e}`)
-    );
-    return message;
+  const lines = []
+  for (const command of Object.keys(commands).toSorted()) {
+    const text = '  ' + command + ' '.repeat(padding - command.length) + commands[command].description;
+    lines.push([text, { type: 'command', pattern: command, url: '' }]);
   }
 
   const preText = {
@@ -86,7 +56,7 @@ function runHelp(term, args) {
   ]}
 
   printContent(term, preText.content);
-  term.writeln(Object.keys(commands).toSorted().map(e => formatMessage(e, commands[e].description)).join('\r\n'));
+  printContent(term, lines);
   printContent(term, postText.content);
 
   code = 0;
