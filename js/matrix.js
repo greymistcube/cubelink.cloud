32e85f8bb2a3c5e7e2a7569b1ae4b249c7c0783e
Say Cheong <greymistcube@gmail.com>
2025-12-28 11:04:49 +0900

    Refactored matrix with classes

--- js/matrix.js
+++ js/matrix.js
@@ -1,95 +1,134 @@
+class Rain {
+  /** Density of the rain */
+  static density = 2;
+
+  /** Base length of a drop */
+  static tail = 8;
+
+  /** Probability of a character glitching */
+  static glitch = 0.1;
+
+  constructor(cols, rows) {
+    this.cols = cols;
+    this.rows = rows;
+    this.count = rows * Rain.density;
+    this.drops = [];
+    this.fill(false);
+  }
+
+  /** Fill up the rain environment with drops */
+  fill(fromTop) {
+    while (this.drops.length < this.count) {
+      this.drops.push(new Drop(
+        Math.floor(Math.random() * cols + 1),
+        fromTop ? 1 : Math.floor(Math.random() * rows + 1)
+      ));
+    }
+  }
+
+  /** Render the scene */
+  render(term) {
+    this.drops.forEach(drop => drop.render(term));
+  }
+
+  /** Make all drops fall by 1 then cycle all drops that are off screen. */
+  fall() {
+    this.drops.forEach(drop => drop.fall());
+    this.drops = this.drops.filter(drop => drop.y < this.rows + drop.chars.length);
+    this.fill(true);
+  }
+}
+
+class Drop {
+  /** Create a drop of random length made of random hex characters */
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.chars = [];
+    const length = Rain.tail + Math.trunc(Math.random() * Rain.tail);
+    while (this.chars.length < length) {
+      this.chars.unshift(Drop.randChar());
+    }
+  }
+
+  /** Draw this drop on the terminal */
+  render(term) {
+    let rows = term.rows;
+    for (let i = 0; i < this.chars.length; i++) {
+      if (Math.random() < Rain.glitch) {
+        this.chars[i] = Drop.randChar();
+      }
+
+      let y = this.y - i;
+      if (1 <= y && y <= rows) {
+        term.write(`\x1b[${y};${this.x}H`);
+        let color = this.getColor(i);
+
+        if (i === 0) {
+          term.write(`${color}${this.chars[i]}\x1b[0m`);
+        } else if (i === this.chars.length - 1) {
+          // Delete the character at the very end of the tail
+          term.write('\x1b[0m ');
+        } else {
+          term.write(`${color}${this.chars[i]}\x1b[0m`);
+        }
+      }
+    }
+  }
+
+  /** Make this drop fall by 1 */
+  fall() {
+    this.y += 1;
+    this.chars.pop();
+    this.chars.unshift(Drop.randChar());
+  }
+
+  /** Get the color for rendering for the character at i */
+  getColor(i) {
+    if (i === 0) {
+      return '\x1b[37;1m'; // Bright white
+    } else if (i < this.chars.length / 3) {
+      return '\x1b[32;1m'; // Bright green
+    } else if (i < this.chars.length * 2 / 3) {
+      return '\x1b[32m'; // Green
+    } else {
+      return '\x1b[32;2m'; // Dimmed green
+    }
+  }
+
+  /** Return a random hex character */
+  static randChar() {
+    return Math.floor(Math.random() * 16).toString(16);
+  }
+}
+
 // This may not behave well on window resize event.
 async function runMatrix(term, args) {
   // Clear the screen and hide the cursor
   term.writeln('', () => { term.clear(); });
   term.write('\x1b[?25l');
 
+  // Turn process to true and add a listener to cancel process from any keypress
   process = true;
   code = 0;
   let listener = term.onData(e => { process = false; });
 
-  let rows = term.rows;
-  let cols = term.cols;
-  const drops = rows; // Tall screens may have many rows
-  const tail = 8; // Actual length is between tail and tail * 2
   const sleep = 64;
-  const glitch = 0.1; // Probability of a character glitch
-  let rain = [];
+  let rain = new Rain(term.cols, term.rows);
 
-  // Creates a random hex character
-  function randChar() {
-    return Math.floor(Math.random() * 16).toString(16);
-  }
-
-  function createDrop() {
-    drop = [];
-    let len = tail + Math.trunc(Math.random() * 8);
-    while (drop.length < len) {
-      drop.unshift(randChar());
-    }
-    return drop;
-  }
-
-  function fillRain(fromTop) {
-    while (rain.length < drops) {
-      rain.push({
-        y: fromTop ? 1 : Math.floor(Math.random() * rows + 1),
-        x: Math.floor(Math.random() * cols + 1),
-        drop: createDrop()
-      });
-    }
-  }
-
-  fillRain(false);
-  while (true) {
-    for (let elem of rain) {
-      for (let i = 0; i < elem.drop.length; i++) {
-        if (Math.random() < glitch) {
-          elem.drop[i] = randChar();
-        }
-
-        let y = elem.y - i;
-        if (1 <= y && y <= rows) {
-          term.write(`\x1b[${y};${elem.x}H`);
-          if (i === 0) {
-            color = '\x1b[37;1m';
-          } else if (i < elem.drop.length / 3) {
-            color = '\x1b[32;1m';
-          } else if (i < elem.drop.length * 2 / 3) {
-            color = '\x1b[32m';
-          } else {
-            color = '\x1b[32;2m';
-          }
-
-          if (i === 0) {
-            term.write(`${color}${elem.drop[i]}\x1b[0m`);
-          } else if (i === elem.drop.length - 1) {
-            term.write('\x1b[0m ');
-          } else {
-            term.write(`${color}${elem.drop[i]}\x1b[0m`);
-          }
-        }
-      }
-
-      elem.y += 1;
-      elem.drop.pop();
-      elem.drop.unshift(randChar());
-    }
-
-    rain = rain.filter(elem => elem.y < rows + elem.drop.length);
-    fillRain(true);
+  while (process) {
+    rain.render(term);
+    rain.fall();
 
     await new Promise(resolve => setTimeout(resolve, sleep));
-    if (process === false) {
-      // Dispose the listener and turn back on the cursor
-      listener.dispose();
-      term.write('\x1b[?25h');
-      break;
-    }
   }
 
+  // Dispose the listener and turn back on the cursor
+  listener.dispose();
+  term.write('\x1b[?25h');
+
   // Move the cursor to the bottom first before clearing
-  term.writeln(`\x1b[${rows};1H`);
+  term.writeln(`\x1b[${term.rows};1H`);
   prompt(term);
   term.write('', () => { term.clear(); });
 }
