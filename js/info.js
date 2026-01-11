197adbae824145e7c237b1805b118dedf17857ea
Say Cheong <greymistcube@gmail.com>
2026-01-11 16:13:03 +0900

    Changed secret names due to GitHub not allowing certain patterns

--- js/info.js
+++ js/info.js
@@ -2,4 +2,4 @@ export const email = { pattern: '<EMAIL_PATTERN>', url: '<EMAIL_URL>' };
 export const tel = { pattern: '<TEL_PATTERN>', url: '<TEL_URL>' };
 export const address = { pattern: '<ADDRESS_PATTERN>', url: '<ADDRESS_URL>' };
 export const homepage = { pattern: '<HOMEPAGE_PATTERN>', url: '<HOMEPAGE_URL>' };
-export const github = { pattern: '<GITHUB_PATTERN>', url: '<GITHUB_URL>' };
+export const github = { pattern: '<GHUB_PATTERN>', url: '<GHUB_URL>' };
